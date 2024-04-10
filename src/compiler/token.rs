1cd7346b5b5764fb41570c6c334bfe9832c944c3
Felix Marcon Swadel <54915877+felix-swadel@users.noreply.github.com>
2024-04-10 14:18:14 +1200

    IRC-1: Add Tokeniser

---
+++ src/compiler/token.rs
@@ -0,0 +1,148 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum Token {
+    // keywords
+    Exit,
+    Let,
+    If,
+    // values
+    Ident(String),
+    Int32(u32),
+    Bool(bool),
+    // maths symbols
+    Plus,
+    Minus,
+    Star,
+    FSlash,
+    // logical operators
+    Equality,
+    NonEquality,
+    GreaterThan,
+    LessThan,
+    And,
+    Or,
+    // control characters
+    Semi,
+    OpenParen,
+    CloseParen,
+    OpenCurly,
+    CloseCurly,
+    Equals,
+}
+
+impl Token {
+    pub fn id(&self) -> TokenId {
+        match self {
+            Token::Exit => TokenId::Exit,
+            Token::Let => TokenId::Let,
+            Token::If => TokenId::If,
+            Token::Ident(_) => TokenId::Ident,
+            Token::Int32(_) => TokenId::Int32,
+            Token::Bool(_) => TokenId::Bool,
+            Token::Plus => TokenId::Plus,
+            Token::Minus => TokenId::Minus,
+            Token::Star => TokenId::Star,
+            Token::FSlash => TokenId::FSlash,
+            Token::Equality => TokenId::Equality,
+            Token::NonEquality => TokenId::NonEquality,
+            Token::GreaterThan => TokenId::GreaterThan,
+            Token::LessThan => TokenId::LessThan,
+            Token::And => TokenId::And,
+            Token::Or => TokenId::Or,
+            Token::Semi => TokenId::Semi,
+            Token::OpenParen => TokenId::OpenParen,
+            Token::CloseParen => TokenId::CloseParen,
+            Token::OpenCurly => TokenId::OpenCurly,
+            Token::CloseCurly => TokenId::CloseCurly,
+            Token::Equals => TokenId::Equals,
+        }
+    }
+
+    pub fn has_prec(&self) -> Option<usize> {
+        match self {
+            Token::Or => Some(0),
+            Token::And => Some(1),
+            Token::Equality | Token::NonEquality |
+            Token::GreaterThan | Token::LessThan => Some(2),
+            Token::Plus | Token::Minus => Some(3),
+            Token::Star | Token::FSlash => Some(4),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Ident(s) => write!(f, "identifier: {}", s),
+            Token::Int32(n) => write!(f, "integer-literal: {}", n),
+            Token::Bool(b) => write!(f, "bool: {}", b),
+            _ => self.id().fmt(f),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokenId {
+    // keywords
+    Exit,
+    Let,
+    If,
+    // values
+    Ident,
+    Int32,
+    Bool,
+    // maths symbols
+    Plus,
+    Minus,
+    Star,
+    FSlash,
+    // logical operators
+    Equality,
+    NonEquality,
+    GreaterThan,
+    LessThan,
+    And,
+    Or,
+    // control characters
+    Semi,
+    OpenParen,
+    CloseParen,
+    OpenCurly,
+    CloseCurly,
+    Equals,
+}
+
+impl fmt::Display for TokenId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TokenId::Exit => "`exit`",
+                TokenId::Let => "`let`",
+                TokenId::If => "`if`",
+                TokenId::Ident => "<identifier>",
+                TokenId::Int32 => "<integer-literal>",
+                TokenId::Bool => "<bool>",
+                TokenId::Plus => "`+`",
+                TokenId::Minus => "`-`",
+                TokenId::Star => "`*`",
+                TokenId::FSlash => "`/`",
+                TokenId::Equality => "`==`",
+                TokenId::NonEquality => "`!=`",
+                TokenId::GreaterThan => "`>`",
+                TokenId::LessThan => "`<`",
+                TokenId::And => "`&&`",
+                TokenId::Or => "`||`",
+                TokenId::Semi => "`;`",
+                TokenId::OpenParen => "`(`",
+                TokenId::CloseParen => "`)`",
+                TokenId::OpenCurly => "`{`",
+                TokenId::CloseCurly => "`}`",
+                TokenId::Equals => "`=`",
+            }
+        )
+    }
+}
---
+++ src/compiler/tokeniser.rs
@@ -0,0 +1,187 @@
+use std::mem;
+use phf::{Map, phf_map};
+
+use super::token::Token;
+
+const KEYWORDS: Map<&'static str, Token> = phf_map! {
+    "exit" => Token::Exit,
+    "let" => Token::Let,
+    "if" => Token::If,
+    "true" => Token::Bool(true),
+    "false" => Token::Bool(false),
+};
+
+#[derive(Debug, PartialEq)]
+enum BufType {
+    Unset,
+    Word,
+    Number,
+}
+
+struct TokenBuf {
+    buf: String,
+    buf_type: BufType,
+}
+
+impl TokenBuf {
+    pub fn new() -> TokenBuf {
+        TokenBuf {
+            buf: String::new(),
+            buf_type: BufType::Unset,
+        }
+    }
+
+    pub fn push(&mut self, c: char) -> Result<(), String> {
+        match self.buf_type {
+            BufType::Unset => {
+                // this is the first character - check for buffer type
+                if c.is_ascii_digit() {
+                    self.buf_type = BufType::Number;
+                } else if c.is_ascii_alphabetic() {
+                    self.buf_type = BufType::Word;
+                } else {
+                    return Err(format!("Unexpected character: {}", c));
+                }
+            }
+            BufType::Number => {
+                // check for digit
+                if !c.is_ascii_digit() {
+                    return Err(format!("Invalid char in number: {}", c));
+                }
+            }
+            BufType::Word => {
+                // check for alphanumeric
+                if !c.is_ascii_alphanumeric() {
+                    return Err(format!("Invalid char in identifier: {}", c));
+                }
+            }
+        }
+        self.buf.push(c);
+        Ok(())
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.buf.is_empty()
+    }
+
+    // utility function to reset buffer and return string
+    fn extract(&mut self) -> String {
+        self.buf_type = BufType::Unset;
+        mem::replace(&mut self.buf, String::new())
+    }
+
+    fn reset(&mut self) {
+        self.buf.clear();
+        self.buf_type = BufType::Unset;
+    }
+
+    pub fn extract_token(&mut self) -> Token {
+        assert_ne!(self.buf_type, BufType::Unset);
+        match self.buf_type {
+            BufType::Number => {
+                let num = match self.buf.parse() {
+                    Ok(num) => num,
+                    Err(_) => panic!("couldn't parse string to u32: {}", self.buf),
+                };
+                self.reset();
+                Token::Int32(num)
+            },
+            BufType::Word => {
+                if let Some(token) = KEYWORDS.get(&self.buf) {
+                    // check if string is keyword
+                    self.reset();
+                    token.clone()
+                } else {
+                    // otherwise assume it's an identifier
+                    Token::Ident(self.extract())
+                }
+            }
+            BufType::Unset => unreachable!(),
+        }
+    }
+}
+
+pub fn tokenise(text: &String) -> Result<Vec<Token>, String> {
+    let mut iter = text.chars().peekable();
+    let mut tokens = Vec::new();
+    let mut buf = TokenBuf::new();
+    // iterate through characters of text
+    while let Some(c) = iter.next() {
+        // check for control characters
+        if c.is_whitespace() {
+            if !(buf.is_empty()) {
+                tokens.push(buf.extract_token());
+            }
+            continue;
+        }
+        let token = match c {
+            // maths symbols
+            '+' => Some(Token::Plus),
+            '-' => Some(Token::Minus),
+            '*' => Some(Token::Star),
+            '/' => Some(Token::FSlash),
+            // logical operators
+            '>' => Some(Token::GreaterThan),
+            '<' => Some(Token::LessThan),
+            // control characters
+            ';' => Some(Token::Semi),
+            '(' => Some(Token::OpenParen),
+            ')' => Some(Token::CloseParen),
+            '{' => Some(Token::OpenCurly),
+            '}' => Some(Token::CloseCurly),
+            '=' => if let Some(next_c) = iter.peek() {
+                if *next_c == '=' {
+                    iter.next();
+                    Some(Token::Equality)
+                } else {
+                    Some(Token::Equals)
+                }
+            } else {
+                Some(Token::Equals)
+            },
+            '!' => if let Some(next_c) = iter.peek() {
+                if *next_c == '=' {
+                    iter.next();
+                    Some(Token::NonEquality)
+                } else {
+                    return Err("unexpected token: `!`".to_owned());
+                }
+            } else {
+                return Err("unexpected token: `!`".to_owned());
+            },
+            '&' => if let Some(next_c) = iter.peek() {
+                if *next_c == '&' {
+                    iter.next();
+                    Some(Token::And)
+                } else {
+                    return Err("unexpected token: `&`".to_owned());
+                }
+            } else {
+                return Err("unexpected token: `&`".to_owned());
+            },
+            '|' => if let Some(next_c) = iter.peek() {
+                if *next_c == '|' {
+                    iter.next();
+                    Some(Token::Or)
+                } else {
+                    return Err("unexpected token: `|`".to_owned());
+                }
+            } else {
+                return Err("unexpected token: `|`".to_owned());
+            },
+            _ => None,
+        };
+        match token {
+            Some(token) => {
+                if !buf.is_empty() {
+                    tokens.push(buf.extract_token());
+                }
+                tokens.push(token);
+            },
+            None => {
+                buf.push(c)?;
+            }
+        }
+    }
+    Ok(tokens)
+}
